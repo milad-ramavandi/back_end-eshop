db923f498af3ee16d37a2de0d76ce8946f0fb088
milad ramavandi <miladramavandi@milads-MacBook-Air.local>
2024-03-08 17:32:00 +0330

    first commit

---
+++ src/main/java/com/api/eshop/controller/ProductsController.java
@@ -0,0 +1,145 @@
+package com.api.eshop.controller;
+
+import com.api.eshop.domain.Products;
+import com.api.eshop.domain.ProductsCategories;
+import com.api.eshop.service.ProductsCategoriesService;
+import com.api.eshop.service.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("products")
+public class ProductsController {
+
+
+    @Autowired
+    private ProductsService service;
+
+    @Autowired
+    private ProductsCategoriesService productsCategoriesService;
+
+    @GetMapping
+    @CrossOrigin("*")
+    public ResponseEntity getAll() {
+        List<Products> result = service.getAll();
+        for (Products product : result) {
+            product.getImages().size();
+        }
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    @CrossOrigin("*")
+    public ResponseEntity getById(@PathVariable long id) {
+        Products result = service.getById(id);
+        if(result!=null) {
+            result.getImages().size();
+            return new ResponseEntity(result, HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity(result , HttpStatus.BAD_REQUEST);
+    }
+
+
+    @GetMapping("name/{name}")
+    @CrossOrigin("*")
+    public ResponseEntity getById(@PathVariable String name) {
+        Products result = service.getByName(name);
+        if(result!=null) {
+            result.getImages().size();
+            return new ResponseEntity(result, HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity(result , HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("incredibleOffers")
+    @CrossOrigin("*")
+    public ResponseEntity getAllIncredibleOffers() {
+        return new ResponseEntity(service.getAllIncredibleOffers(), HttpStatus.OK);
+    }
+
+    @GetMapping("categories")
+    @CrossOrigin("*")
+    public ResponseEntity getAllCategories() {
+        List<ProductsCategories> result = productsCategoriesService.getAll();
+        for (ProductsCategories item : result) {
+            item.getProducts().size();
+        }
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
+
+    @GetMapping("dailySuggests")
+    @CrossOrigin("*")
+    public ResponseEntity getAllDailySuggests() {
+        List<Products> result = service.getAllDailySuggests();
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
+
+    @GetMapping("search/{textToSearch}")
+    @CrossOrigin("*")
+    public ResponseEntity getAllDailySuggests(@PathVariable String textToSearch) {
+        List<Products> result = service.searchProducts(textToSearch);
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
+
+    @GetMapping("category/{categoryName}")
+    @CrossOrigin("*")
+    public ResponseEntity getProductsByCategoryName(@PathVariable String categoryName) {
+
+         ProductsCategories result = productsCategoriesService.getByName(categoryName);
+       try {
+           result.getProducts().size();
+       }
+       catch (Exception e)
+       {
+
+       }
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
+
+
+    @GetMapping("KharideSareMah")
+    public ResponseEntity getKharideSareMahProducts()
+    {
+        return new ResponseEntity(service.getKharideSareMahProducts() , HttpStatus.OK);
+    }
+
+    @GetMapping("JameJahani")
+    public ResponseEntity getJameJahaniProducts()
+    {
+        return new ResponseEntity(service.getJameJahaniProducts() , HttpStatus.OK);
+    }
+
+    @GetMapping("PishnahadeRoozeMobile")
+    public ResponseEntity getPishnahadeRoozeMobileProducts()
+    {
+        return new ResponseEntity(service.getPishnahadeRoozeMobileProducts() , HttpStatus.OK);
+    }
+
+    @GetMapping("ForoosheOmdeh")
+    public ResponseEntity getForoosheOmdehProducts()
+    {
+        return new ResponseEntity(service.getForoosheOmdehProducts() , HttpStatus.OK);
+    }
+    @GetMapping("KharideAghsati")
+    public ResponseEntity getKharideAghsatiProducts()
+    {
+        return new ResponseEntity(service.getKharideAghsatiProducts() , HttpStatus.OK);
+    }
+
+    @GetMapping("Harajestoon")
+    public ResponseEntity getHarajestoonProducts()
+    {
+        return new ResponseEntity(service.getHarajestoonProducts() , HttpStatus.OK);
+    }
+    @GetMapping("DigiJet")
+    public ResponseEntity getDigiJetProducts()
+    {
+        return new ResponseEntity(service.getDigiJetProducts() , HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/api/eshop/domain/ProductsCategories.java
@@ -0,0 +1,28 @@
+package com.api.eshop.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductsCategories {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String image;
+    private String title;
+
+    @OneToMany(mappedBy = "category" , cascade = CascadeType.ALL)
+    @JsonManagedReference("category")
+    private List<Products> products;
+}
